143bdd09f445802cc2e15870a889b7b5286c2aa4
Pierre-Louis Janand <26655725+Pilouche@users.noreply.github.com>
2023-04-26 18:52:30 +0900

    refactor:new StarkNet version

--- src/python/connector/connector_utils.py
+++ src/python/connector/connector_utils.py
@@ -1,5 +1,5 @@
 from starknet_py.net.account.account import Account
-from starknet_py.net import KeyPair
+from starknet_py.net.signer.stark_curve_signer import KeyPair
 from starknet_py.net.gateway_client import GatewayClient
 from starknet_py.net.models.chains import StarknetChainId
 
